14dcf75ba0722811cee1e1fa2ffbaa7562860053
Haigutus <vilgo44@hotmail.com>
2023-03-23 05:55:42 +0200

    Initial state from https://github.com/Haigutus/USVDM

---
+++ rdf_tools/__init__.py
@@ -0,0 +1,6 @@
+from rdf_parser import RDF_parser
+from rdf_parser import CGMES_tools
+from rdf_parser import RDFS_tools
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
