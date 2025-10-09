7b15a39412076f07e38f926b36924d79559629dd
Haigutus <vilgo44@hotmail.com>
2025-10-09 20:02:10 +0300

    Added sphinx based documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # Points to the project root
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'triplets'
+copyright = '2025, Kristjan Vilgo'
+author = 'Kristjan Vilgo'
+release = '0.0.11'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',      # Automatically document modules and functions
+    'sphinx.ext.napoleon',     # Support for NumPy-style docstrings
+    'sphinx.ext.viewcode',     # Add links to source code
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
---
+++ docs/source/triplets.rdfs_tools.rst
@@ -0,0 +1,29 @@
+triplets.rdfs\_tools package
+============================
+
+Submodules
+----------
+
+triplets.rdfs\_tools.rdfs\_to\_json module
+------------------------------------------
+
+.. automodule:: triplets.rdfs_tools.rdfs_to_json
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+triplets.rdfs\_tools.rdfs\_tools module
+---------------------------------------
+
+.. automodule:: triplets.rdfs_tools.rdfs_tools
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: triplets.rdfs_tools
+   :members:
+   :show-inheritance:
+   :undoc-members:
