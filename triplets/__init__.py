482b22cbaa5c66665c3906ec086103700c99cc83
Haigutus <vilgo44@hotmail.com>
2025-10-11 14:18:56 +0300

    Changed how import work to main module

--- triplets/__init__.py
+++ triplets/__init__.py
@@ -3,11 +3,11 @@
 #from triplets import rdfs_tools
 
 # Define public API
-#__all__ = ['cgmes_tools', 'rdf_parser']
+__all__ = ['cgmes_tools', 'rdf_parser']
 
 # Import modules explicitly for package namespace
-#from . import cgmes_tools
-#from . import rdf_parser
+from . import cgmes_tools
+from . import rdf_parser
 
 from ._version import get_versions
 __version__ = get_versions()['version']
