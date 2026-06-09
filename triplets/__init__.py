4dfce4913e7984cffb3ca2be89e674a1d5b12a90
Kristjan Vilgo <kristjan.vilgo@gmail.com>
2026-06-09 20:03:25 +0300

    feat: register df.triplets.* accessor namespace for pandas and polars (Phase 3)

--- triplets/__init__.py
+++ triplets/__init__.py
@@ -6,6 +6,7 @@ from . import rdfs_tools
 from . import cli
 from . import tools
 from . import export
+from . import _accessor  # registers df.triplets.* namespace  # noqa: F401
 
 __all__ = [
     'cgmes_tools',
